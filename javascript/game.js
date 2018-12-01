e68f3139dbcedaccf4bb9b2abcfdd8db8edb0488
David Byrd <dbyrd@depparts.com>
2018-12-01 15:31:40 -0500

    game working

--- javascript/game.js
+++ javascript/game.js
@@ -3,34 +3,25 @@ $(document).ready(function() {
 var gameTotal = 0;
 var wins = 0;
 var losses = 0;
-
- // Generates a random number between 19 and 120 as the target number
 var randomTotal = Math.floor(Math.random() * 101) + 19;
 
-// Generates a random value for each gem between 1 and 12
-var gem1 = Math.floor(Math.random() * 12) + 1;
-console.log(gem1);
+    var gem1 = Math.floor(Math.random() * 12) + 1;
 
-var gem2 = Math.floor(Math.random() * 12) + 1;
-console.log(gem2)
+    var gem2 = Math.floor(Math.random() * 12) + 1;
 
-var gem3 = Math.floor(Math.random() * 12) + 1;
-console.log(gem3)
+    var gem3 = Math.floor(Math.random() * 12) + 1;
 
-var gem4 = Math.floor(Math.random() * 12) + 1;
-console.log(gem4)
-
-function reset() {
-    $("#target, #total").empty()
-}
+    var gem4 = Math.floor(Math.random() * 12) + 1;
 
 // Starts the game when clicked
 $("#start").on("click", function() {
-    $("#target").text(randomTotal);
-});
+    
+    
 
-// If a crystal is clicked, it's randomly assigned value is added to the user's total
-$("#gem1").on("click", function() {
+    $("#target").text(randomTotal);
+    
+    // If a crystal is clicked, it's randomly assigned value is added to the user's total
+    $("#gem1").on("click", function() {
     gameTotal += gem1;
     $("#total").text(gameTotal);
     // If the total reaches exactly the random number, the user wins, the game resets, and a win is added to the win counter
@@ -91,6 +82,28 @@ $("#gem4").on("click", function() {
         reset();
     }
 });
+
+});
+
+function reset() {
+    $("#target, #total").empty()
+    randomTotal = Math.floor(Math.random() * 101) + 19;
+
+    gem1 = Math.floor(Math.random() * 12) + 1;
+    console.log(gem1);
+
+    gem2 = Math.floor(Math.random() * 12) + 1;
+    console.log(gem2)
+
+    gem3 = Math.floor(Math.random() * 12) + 1;
+    console.log(gem3)
+
+    gem4 = Math.floor(Math.random() * 12) + 1;
+    console.log(gem4)
+
+    gameTotal = 0;
+    $("#target").text(randomTotal);
+}
 
 });
 
