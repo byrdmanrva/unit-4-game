68cd0ff604358251300ff9ca9975335658213dac
David Byrd <dbyrd@depparts.com>
2018-12-01 09:26:16 -0500

    backup

--- javascript/game.js
+++ javascript/game.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
-// var total = [];
-// var isEqual = false;
-// var isOver = false;
-var gemValue = [];
+var gameTotal = 0;
+var wins = 0;
+var losses = 0;
 
-// Assign random values between 1 and 12 to each crystal 
+ // Generates a random number between 19 and 120 as the target number
+var randomTotal = Math.floor(Math.random() * 101) + 19;
+
 // Generates a random value for each gem between 1 and 12
 var gem1 = Math.floor(Math.random() * 12) + 1;
 console.log(gem1);
@@ -19,55 +20,80 @@ console.log(gem3)
 var gem4 = Math.floor(Math.random() * 12) + 1;
 console.log(gem4)
 
+function reset() {
+    $("#target, #total").empty()
+}
 
 // Starts the game when clicked
 $("#start").on("click", function() {
-// $("target").empty();
- // Generates a random number between 19 and 120 as the target number
-var randomTotal = Math.floor(Math.random() * 101) + 19;
-console.log(randomTotal);
-$("#target").text(randomTotal);
-
+    $("#target").text(randomTotal);
 });
-// If a crystal is clicked, it's randomly assigned value is added to the user's total
 
+// If a crystal is clicked, it's randomly assigned value is added to the user's total
 $("#gem1").on("click", function() {
-$("#total").text(gem1);
+    gameTotal += gem1;
+    $("#total").text(gameTotal);
+    // If the total reaches exactly the random number, the user wins, the game resets, and a win is added to the win counter
+    if (gameTotal === randomTotal) {
+        wins++;
+        $("#counter").text("Wins " + wins);
+        reset();
+    }
+    // If the total goes over the random number, the user loses, the game resets, and a loss is added to the loss counter
+    else if (gameTotal > randomTotal) {
+        losses++;
+        $("#counter2").text("Losses " + losses);
+        reset();
+    }
 });
 
 $("#gem2").on("click", function() {
-$("#total").text(gem2);
+    gameTotal += gem2;
+    $("#total").text(gameTotal);
+    if (gameTotal === randomTotal) {
+        wins++;
+        $("#counter").text("Wins " + wins);
+        reset();
+    }
+    else if (gameTotal > randomTotal) {
+        losses++;
+        $("#counter2").text("Losses " + losses);
+        reset();
+    }
 });
 
 $("#gem3").on("click", function() {
-$("#total").text(gem3);
+    gameTotal += gem3;
+    $("#total").text(gameTotal);
+    if (gameTotal === randomTotal) {
+        wins++;
+        $("#counter").text("Wins " + wins);
+        reset();
+    }
+    else if (gameTotal > randomTotal) {
+        losses++;
+        $("#counter2").text("Losses " + losses);
+        reset();
+    }
 });
 
 $("#gem4").on("click", function() {
-$("#total").text(gem4);
+    gameTotal += gem4;
+    $("#total").text(gameTotal);
+    if (gameTotal === randomTotal) {
+        wins++;
+        $("#counter").text("Wins " + wins);
+        reset();
+    }
+    else if (gameTotal > randomTotal) {
+        losses++;
+        $("#counter2").text("Losses " + losses);
+        reset();
+    }
 });
 
-// If the total reaches exactly the random number, the user wins, the game resets, and a win is added to the win counter
-
-
-// If the total goes over the random number, the user loses, the game resets, and a loss is added to the loss counter
-
-
-
-
 });
 
-// var randomTotal = total[Math.floor(Math.random() * total.length)];
-// console.log(randomTotal);
-// $("#target").text(randomTotal);
-
-// for (var i = 1; i < 13; i++) {
-//     gemValue.push([i]);
-// };
-
-// $("#target").text(randomTotal);
-// window.onkeypress = function gameStart (){
-//     $("#target").text(randomTotal);};
 
 
 
